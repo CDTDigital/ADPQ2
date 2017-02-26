9e3213bd48d7cc6749817652bccc536c345564ef
thomas <tbertran@natomatech.com>
2017-02-26 11:30:20 -0800

    minor fixes to admin nav

--- src/Com.Natoma.Adpq.Prototype.Web/App/admin/admin-home.component.ts
+++ src/Com.Natoma.Adpq.Prototype.Web/App/admin/admin-home.component.ts
@@ -23,8 +23,6 @@ export class AdminHomeComponent implements OnInit {
         if (user.isAdmin == false)
             this.router.navigate(["./user"]);
 
-        this.userService.getLoggedInUser();
-
         this.adpqService.breadcrumbItems = [<MenuItem> { label: 'Admin Home', routerLink: ['./admin'] }];
     }
 
