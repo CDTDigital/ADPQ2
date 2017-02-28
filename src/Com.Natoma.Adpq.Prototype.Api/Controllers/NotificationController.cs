11596a503449b92d020ed5064bd1cbcd526d26b5
briley-natomatech <briley@natomatech.com>
2017-02-27 18:03:44 -0800

    adding admin based role auth for notification controller.  normalizing projects to core 1.0.1

--- src/Com.Natoma.Adpq.Prototype.Api/Controllers/NotificationController.cs
+++ src/Com.Natoma.Adpq.Prototype.Api/Controllers/NotificationController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Com.Natoma.Adpq.Prototype.Business.Models.Notification;
 using Com.Natoma.Adpq.Prototype.Business.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,18 +21,21 @@ namespace Com.Natoma.Adpq.Prototype.Api.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "Admin")]
         public IActionResult Get()
         {
             return Ok(_notificationService.Get());
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public IActionResult Get([FromRoute]int id)
         {
             return Ok(_notificationService.Get(id));
         }
 
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Post([FromBody]NotificationViewModel notificationViewModel)
         {
             return Ok(await _notificationService.CreateAndSendNotification(notificationViewModel));
