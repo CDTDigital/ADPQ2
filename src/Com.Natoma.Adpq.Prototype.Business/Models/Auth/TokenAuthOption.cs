e4d91c321a4fe797af473703e688412bff3cfa8b
briley-natomatech <briley@natomatech.com>
2017-03-01 21:36:58 -0800

    removing dependency to system.cryptography and RSA

--- src/Com.Natoma.Adpq.Prototype.Business/Models/Auth/TokenAuthOption.cs
+++ src/Com.Natoma.Adpq.Prototype.Business/Models/Auth/TokenAuthOption.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using Com.Natoma.Adpq.Prototype.Business.Utils;
+﻿using System.Text;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Com.Natoma.Adpq.Prototype.Business.Models.Auth
 {
     public class TokenAuthOption
     {
-        public static string Audience { get; } = "https://exmample.com";
+        public static string Audience { get; } = "https://example.com";
         public static string Issuer { get; } = "noreply@example.com";
-        public static RsaSecurityKey Key { get; } = new RsaSecurityKey(RSAKeyHelper.GenerateKey());
-        public static SigningCredentials SigningCredentials { get; } = new SigningCredentials(Key, SecurityAlgorithms.RsaSha256Signature);
+        public static SecurityKey Key { get; } = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("qhMIqDeTb5v31ZM3nyKo"));
+        public static SigningCredentials SigningCredentials { get; } = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
         public static string TokenType { get; } = "Bearer";
     }
 }
