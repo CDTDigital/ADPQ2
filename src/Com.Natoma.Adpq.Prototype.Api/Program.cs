a39312e42f273b2a6a1b756d5d891554d717618a
Veronica Westlund <vwestlund@natomatech.com>
2017-03-02 17:20:56 -0800

    Changing to use port 5050 in all cases for API, and updated for all environments.

--- src/Com.Natoma.Adpq.Prototype.Api/Program.cs
+++ src/Com.Natoma.Adpq.Prototype.Api/Program.cs
@@ -12,6 +12,7 @@ namespace Com.Natoma.Adpq.Prototype.Api
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5050")
                 .Build();
 
             host.Run();
